a3a54ea9c962bf7b9b4cf02bc15bba0b6d4fef8f
aim-dev-apps <albertoinesbusiness@gmail.com>
2025-08-15 13:23:31 +0200

    Update renfe_scraper.py

--- renfe_scraper.py
+++ renfe_scraper.py
@@ -35,12 +35,12 @@ from webdriver_manager.chrome import ChromeDriverManager
 # ==============================================================================
 
 # -- Datos del Viaje --
-ORIGEN = "Linares-Baeza"
-DESTINO = "Madrid"
+ORIGEN = "Madrid"
+DESTINO = "Barcelona"
 # Formato: "DD/MM/AAAA"
 FECHA_IDA = "17/08/2025"
 # Formato: "HH:MM"
-HORA_SALIDA_TREN = "09:13"
+HORA_SALIDA_TREN = "07:27"
 
 # -- Datos del Email de Notificación --
 EMAIL_REMITENTE = os.environ.get("EMAIL_REMITENTE")
@@ -270,3 +270,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
